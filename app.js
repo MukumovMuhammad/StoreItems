c3ccb75af6d0fffce3af316a5a66a5ce38c1d2aa
Muhammad <muhammad.bw.03@gmail.com>
2023-07-13 18:12:32 +0500

    if - delete

--- app.js
+++ app.js
@@ -4,18 +4,17 @@ const hostname = 'localhost';
 const PORT = 3000;
 
 const server = http.createServer(function (req, res) {
-  if (req.url === '/') {
-    // Serve the HTML file
-    fs.readFile('index.html', function (err, data) {
-      if (err) console.log(err);
-      res.writeHead(200, { 'Content-Type': 'text/html' });
-      console.log("HTML edit!!");
-      res.write(data);
-      res.end();
-    });
+  // Serve the HTML file
+  fs.readFile('index.html', function (err, data) {
+    if (err) console.log(err);
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    console.log("HTML edit!!");
+    res.write(data);
+    res.end();
+  });
 
 
-  } else if (req.url === '/style.css') {
+  if (req.url === '/style.css') {
     // Serve the CSS file
     fs.readFile('style.css', function (err, data) {
       res.writeHead(200, { 'Content-Type': 'text/css' });
